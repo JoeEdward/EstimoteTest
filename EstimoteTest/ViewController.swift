b51db6aa215ad9e2f9b6e2b1a1eaea4ff9b15166
Joe Edwards <maddo4.dev@gmail.com>
2017-10-11 09:31:59 +0100

    Initial Commit

---
+++ EstimoteTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EstimoteTest
+//
+//  Created by Joe Edwards on 11/10/2017.
+//  Copyright © 2017 Maddo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
